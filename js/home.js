33dae9773f26c4136e68172d9d5238f68f5fde75
Paresh Bhagat <pareshbhagat29@gmail.com>
2023-06-04 12:54:09 +0530

    Add radio actions to show and hide sections
    Signed-off-by: Paresh Bhagat <pareshbhagat29@gmail.com>

--- js/home.js
+++ js/home.js
@@ -1,8 +1,21 @@
+// jquery for section hide and show on radio button
+
 $(document).ready(function(){
+
     $('input[type="radio"]').click(function(){
         var inputValue = $(this).attr("value");
-        var targetBox = $("." + inputValue);
-        $(".box").not(targetBox).hide();
-        $(targetBox).show();
+
+        if (inputValue=="all") {
+            $(".box").show("slow","swing");
+        }
+        else
+        {
+            var targetBox = $("." + inputValue);
+            $(".box").not(targetBox).hide("slow","swing");
+            $(targetBox).show("slow","swing");
+        }
+        
     });
+
 });
+
